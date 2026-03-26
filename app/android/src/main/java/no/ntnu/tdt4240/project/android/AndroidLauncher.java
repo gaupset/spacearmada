7437e19784c7c3b7b30d9533bef4e0d549abca6a
Mats Bakketeig <mats.bakketeig@outlook.com>
2026-03-26 15:40:06 +0100

    add new empty project

---
+++ app/android/src/main/java/no/ntnu/tdt4240/project/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package no.ntnu.tdt4240.project.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.ntnu.tdt4240.project.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
