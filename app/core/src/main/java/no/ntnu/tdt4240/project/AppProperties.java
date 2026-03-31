ce887d1eabf3353a5246fdc90cbe38d855dc9702
Mats Bakketeig <mats.bakketeig@outlook.com>
2026-03-31 19:01:50 +0200

    rename AppConfig to AppProperties

--- app/core/src/main/java/no/ntnu/tdt4240/project/AppProperties.java
+++ app/core/src/main/java/no/ntnu/tdt4240/project/AppProperties.java
@@ -1,6 +1,6 @@
 package no.ntnu.tdt4240.project;
 
-public class AppConfig {
+public class AppProperties {
     public static final int WIDTH = 480;
     public static final int HEIGHT = 800;
     public static final String TITLE = "Space Armada";
--- app/lwjgl3/src/main/java/no/ntnu/tdt4240/project/lwjgl3/Lwjgl3Launcher.java
+++ app/lwjgl3/src/main/java/no/ntnu/tdt4240/project/lwjgl3/Lwjgl3Launcher.java
@@ -3,7 +3,7 @@ package no.ntnu.tdt4240.project.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-import no.ntnu.tdt4240.project.AppConfig;
+import no.ntnu.tdt4240.project.AppProperties;
 import no.ntnu.tdt4240.project.Main;
 
 /** Launches the desktop (LWJGL3) application. */
@@ -19,7 +19,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle(AppConfig.TITLE);
+        configuration.setTitle(AppProperties.TITLE);
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
@@ -30,7 +30,7 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setWindowedMode(AppConfig.WIDTH, AppConfig.HEIGHT);
+        configuration.setWindowedMode(AppProperties.WIDTH, AppProperties.HEIGHT);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
